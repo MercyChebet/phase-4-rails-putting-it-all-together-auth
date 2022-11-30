d0879fcdd1ecd78d08976b9f07c503ba665639d0
MercyChebet <chebetm96@gmail.com>
2022-11-30 14:11:53 +0300

    Completed Assignment

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
+  include ActionController::Cookies
+
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
+  
+  def unprocessable_entity_method e
+    render json: {errors: [e.record.errors]}, status: :unprocessable_entity
+  end
 end
